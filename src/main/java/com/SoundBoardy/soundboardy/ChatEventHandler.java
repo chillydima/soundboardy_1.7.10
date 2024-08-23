2df4b3209c8e1c15994fc647a36f131ca0f8ec05
Luksandr <hkhmmm@yandex.ru>
2024-08-23 23:39:02 +0300

    removed unnecessary logs

--- src/main/java/com/SoundBoardy/soundboardy/ChatEventHandler.java
+++ src/main/java/com/SoundBoardy/soundboardy/ChatEventHandler.java
@@ -14,8 +14,6 @@ public class ChatEventHandler {
         EntityPlayer hto = cli.getClientPlayerEntity();
         String msg = event.message.getUnformattedText();
 
-        Main.LOG.info(msg);
-        Main.LOG.info(hto.getDisplayName());
 
         if (msg
             .split("> ").length != 0 || msg.split("] ").length != 0) {
@@ -23,20 +21,16 @@ public class ChatEventHandler {
                 String msg1;
                 msg = msg.split("> ")[msg.split("> ").length - 1];
                 msg1 = msg.split("] ")[msg.split("] ").length - 1];
-                // Main.LOG.info(msg);
                 if (msg.split(" ").length != 1 && msg1.split(" ").length != 1) {
-                    Main.LOG.info("neither fits");
                     return;
                 }
                 if (msg.split(" ").length == 1) {
-                    Main.LOG.info("попытка запустить звук: " + Main.MODID + ":" + msg);
                     cli.getClient().getSoundHandler().playSound(new MovingSoundPlayer(hto, Main.MODID + ":" + msg));
 
 
                     return;
                 }
                 if (msg1.split(" ").length == 1) {
-                    Main.LOG.info("попытка запустить звук: " + Main.MODID + ":" + msg1);
                     cli.getClient().getSoundHandler().playSound(new MovingSoundPlayer(hto, Main.MODID + ":" + msg1));
                 }
             } catch (ArrayIndexOutOfBoundsException mogus) {
